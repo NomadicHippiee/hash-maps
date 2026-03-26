774c5b0c333a99d7f0f50a987ba71f98d08f26ff
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2026-03-26 20:01:48 +0100

    Add hash, set and get method to HashMap class

---
+++ hashMaps.js
@@ -0,0 +1,106 @@
+class Node {
+    constructor(key = null, value = null, next = null) {
+        this.key = key;
+        this.value = value; 
+        this.next = next;
+    }
+}
+
+class HashMap {
+    constructor() {
+        this.capacity = 16;
+        this.buckets = [];
+        this.loadFactor = 0.75;
+        this.size = 0;
+
+        for (let i = 0; i < this.capacity; i++) {
+            this.buckets[i] = null;
+        }
+    }
+
+    hash(key) {
+        let hashCode = 0;
+        let primeNumber = 31;
+        let capacity = this.buckets.length;
+
+        for (let i = 0; i < key.length; i++) {
+            let charCode = key.charCodeAt(i);
+            hashCode = (primeNumber * hashCode) + charCode;
+            hashCode = hashCode % capacity;
+
+
+        }
+
+        return hashCode;
+    }
+
+    set(key, value) {
+        if (typeof key !== 'string') {
+            throw new Error("Keys must be strings")
+        }
+
+        let index = this.hash(key);
+
+        if (index < 0 || index >= this.buckets.length) {
+            throw new Error("Trying to access index out of bounds");
+        }
+
+        if (this.buckets[index] === null) {
+            let newNode = new Node(key, value, null);
+            this.buckets[index] = newNode;
+            this.size = this.size + 1;
+            return;
+        }
+
+        let currentNode = this.buckets[index];
+
+        while (currentNode !== null) {
+            if (currentNode.key === key) {
+                currentNode.value = value;
+                return;
+            }
+
+            if (currentNode.next === null) {
+                let newNode = new Node(key, value, null);
+                currentNode.next = newNode;
+                this.size = this.size + 1
+                break;
+            }
+
+            currentNode = currentNode.next;
+        }
+
+        // if (this.size > (this.capacity * this.loadFactor)) {
+        //     this.grow();
+        // }
+
+    }
+    get(key) {
+        if (typeof key !== 'string') {
+            throw new Error ("Keys must be strings");
+        }
+
+        let index = this.hash(key);
+
+        if (index < 0 || index >= this.buckets.length) {
+            throw new Error("Trying to access index out of bounds");
+        }
+
+        if (this.buckets[index] === null) {
+            return null;
+        }
+
+        let currentNode = this.buckets[index];
+
+        while (currentNode !== null) {
+            if (currentNode.key === key) {
+                return currentNode.value;
+            }
+
+            currentNode = currentNode.next;
+        }
+
+        return null;
+    }
+}
+
